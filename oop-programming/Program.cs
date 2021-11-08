21da9be7d542f44620ba8da6e14c5a47db7c9cfd
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-08 20:22:44 +0700

    Create Program.cs

---
+++ oop-programming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
