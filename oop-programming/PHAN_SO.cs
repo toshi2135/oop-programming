561f5c82ad73ae0fd1ff612bbd229df9dae1e82d
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 02:15:06 +0700

    Create PHAN_SO.cs
    Create Class PHAN_SO

---
+++ oop-programming/PHAN_SO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace oop_programming
+{
+    public class PHAN_SO
+    {
+        // Thanh phan du lieu
+        public int TuSo;
+        public int MauSo;
+
+        // Thanh phan xu li
+        public void Nhap(string ghiChu)
+        {
+            Console.WriteLine(ghiChu);
+            Console.WriteLine("Nhap tu so: ");
+            // this = Doi tuong de goi ham/thuc hien hanh dong
+            this.TuSo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap mau so: ");
+            this.TuSo = int.Parse(Console.ReadLine());
+        }
+    }
+}
