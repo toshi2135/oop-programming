008ce979ad0cadf1235ee8e46fb7794520c730c6
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 03:21:37 +0700

    Create DA_GIAC.cs

---
+++ oop-programming/DA_GIAC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace oop_programming
+{
+    public class DA_GIAC
+    {
+        // Thanh phan du lieu
+        public int n;
+
+        // Thanh phan xu li
+        public void NhapSoDinh(string ghiChu)
+        {
+            Console.WriteLine(ghiChu);
+            Console.WriteLine("Nhap so dinh N: ");
+            this.n = int.Parse(Console.ReadLine());
+        }
+    }
+}
