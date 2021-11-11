f7b523cb267b3022b5135a363efccf3c8fc716c5
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 03:38:27 +0700

    Update DA_GIAC.cs

--- oop-programming/DA_GIAC.cs
+++ oop-programming/DA_GIAC.cs
@@ -4,14 +4,32 @@ namespace oop_programming
     public class DA_GIAC
     {
         // Thanh phan du lieu
-        public int n;
+        public TOA_DO[] dsDinh;
 
         // Thanh phan xu li
         public void NhapSoDinh(string ghiChu)
         {
             Console.WriteLine(ghiChu);
             Console.WriteLine("Nhap so dinh N: ");
-            this.n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            this.dsDinh = new TOA_DO[n];
+            for (int i=0; i < this.dsDinh.Length; i++)
+            {
+                this.dsDinh[i] = new TOA_DO();
+                this.dsDinh[i].Nhap($"Nhap diem A[{i}]: ");
+            }
+        }
+
+        public double TinhChuVi()
+        {
+            double kq = 0;
+            for (int i=0; i<this.dsDinh.Length-1; i++)
+            {
+                kq += this.dsDinh[i].TinhKhoangCach(this.dsDinh[i + 1]);
+            }
+            kq += this.dsDinh[0].TinhKhoangCach(this.dsDinh[dsDinh.Length - 1]); ;
+
+            return kq;
         }
     }
 }
