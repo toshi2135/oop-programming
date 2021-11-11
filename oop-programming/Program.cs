39b7d0e20e26e447cbfdd7566f4675c0160af2dd
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 02:14:50 +0700

    Update Program.cs as OOP

--- oop-programming/Program.cs
+++ oop-programming/Program.cs
@@ -6,6 +6,9 @@ namespace oop_programming
     {
         static void Main(string[] args)
         {
+            // Viet duoi dang Ham (functional)
+
+            /*
             PHANSO a, b;
             a = XL_PHANSO.NhapPhanSo("Nhap phan so a:");
             b = XL_PHANSO.NhapPhanSo("Nhap phan so b:");
@@ -16,6 +19,23 @@ namespace oop_programming
             string chuoi = "Ket qua la " + XL_PHANSO.Xuat(s);
 
             Console.WriteLine(chuoi);
+            */
+
+            // Viet duoi dang HDT (oop)
+
+            // B1. Khai bao & tao lap doi tuong
+            PHAN_SO a, b; // Khai bao
+            a = new PHAN_SO(); // Tao lap
+            b = new PHAN_SO();
+
+            // B2. Yeu cau doi tuong thuc hien hanh dong
+            a.Nhap("Nhap phan so a: ");
+            b.Nhap("Nhap phan so b: ");
+
+            // PHAN_SO s;
+            // s = a.CongPhanSo(b);
+            // string chuoi = "Ket qua la " + s.Xuat();
+            // Console.WriteLine(chuoi);
         }
     }
 }
