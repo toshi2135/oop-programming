8ffa122c2f4c7ef932be93c6f225f5b5e644d32c
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 02:44:02 +0700

    Update Program.cs

--- oop-programming/Program.cs
+++ oop-programming/Program.cs
@@ -22,7 +22,7 @@ namespace oop_programming
             */
 
             // Viet duoi dang HDT (oop)
-
+            /*
             // B1. Khai bao & tao lap doi tuong
             PHAN_SO a, b; // Khai bao
             a = new PHAN_SO(); // Tao lap
@@ -32,10 +32,25 @@ namespace oop_programming
             a.Nhap("Nhap phan so a: ");
             b.Nhap("Nhap phan so b: ");
 
-            // PHAN_SO s;
-            // s = a.CongPhanSo(b);
-            // string chuoi = "Ket qua la " + s.Xuat();
-            // Console.WriteLine(chuoi);
+            PHAN_SO s;
+            s = new PHAN_SO();
+            s = a.CongPhanSo(b);
+            string chuoi = "Ket qua la " + s.Xuat();
+            Console.WriteLine(chuoi);
+            */
+
+            // Tinh khoang cach 2 diem/toa do - OOP
+            TOA_DO a, b;
+            a = new TOA_DO();
+            b = new TOA_DO();
+
+            a.Nhap("Nhap diem A: ");
+            b.Nhap("Nhap diem B: ");
+
+            double kq;
+            kq = a.TinhKhoangCach(b);
+            string chuoi = "Ket qua la " + kq;
+            Console.WriteLine(chuoi);
         }
     }
 }
