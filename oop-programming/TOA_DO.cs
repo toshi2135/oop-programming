6c98fe2d796a548d19b8a46b377d929af0bd4d3c
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 02:44:00 +0700

    Create TOA_DO.cs
    Create Class TOA_DO in OOP

---
+++ oop-programming/TOA_DO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace oop_programming
+{
+    public class TOA_DO
+    {
+        // Thanh phan du lieu
+        public int x;
+        public int y;
+
+        // Thanh phan xu li
+        public void Nhap(string ghiChu)
+        {
+            Console.WriteLine(ghiChu);
+            Console.WriteLine("Nhap x: ");
+            this.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap y: ");
+            this.y = int.Parse(Console.ReadLine());
+        }
+
+        public double TinhKhoangCach(TOA_DO b)
+        {
+            double kq;
+            kq = Math.Sqrt(Math.Pow(this.x - b.x, 2) + Math.Pow(this.y - b.y, 2));
+            return kq;
+        }
+
+        
+    }
+}
