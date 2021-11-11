f3a04dedb9ee2167b30233ae3a0f7fdbf7bdd1c6
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-12 01:19:58 +0700

    Create XL_PHANSO.cs

---
+++ oop-programming/XL_PHANSO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace oop_programming
+{
+    public struct PHANSO
+    {
+        public int TuSo;
+        public int MauSo;
+    }
+    public class XL_PHANSO
+    {
+        // Cac ham lien quan xu ly phan so
+        public static PHANSO NhapPhanSo(string ghiChu)
+        {
+            PHANSO kq;
+            Console.WriteLine(ghiChu);
+            Console.WriteLine("Nhap tu so: ");
+            kq.TuSo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap mau so: ");
+            kq.MauSo = int.Parse(Console.ReadLine());
+            return kq;
+        }
+
+        public static PHANSO TinhTongPhanSo(PHANSO a, PHANSO b)
+        {
+            PHANSO kq;
+            kq.TuSo = a.TuSo * b.MauSo + a.MauSo * b.TuSo;
+            kq.MauSo = a.MauSo * b.MauSo;
+            return kq;
+        }
+
+        public static string Xuat(PHANSO a)
+        {
+            return $"{a.TuSo}/{a.MauSo}";
+        }
+    }
+}
