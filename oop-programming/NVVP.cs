53cb208c773240ff53e128f0903766b958760f11
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-14 16:01:34 +0700

    Update NVVP.cs
    Update for inheritance

--- oop-programming/NVVP.cs
+++ oop-programming/NVVP.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 namespace oop_programming
 {
-    public class NVVP
+    public class NVVP : NHANVIEN
     {
         // Thanh phan du lieu
-        private string ma;
-        private string ten;
-        private string diaChi;
         private int phuCap;
         private double heSo;
 
         // Constructor
+        /*
         public NVVP(string maNV, string tenNV, string diaChiNV, int phuCapNV, double heSoNV)
         {
             this.ma = maNV;
@@ -19,44 +17,16 @@ namespace oop_programming
             this.phuCap = phuCapNV;
             this.heSo = heSoNV;
         }
+        */
 
         // Properties
-        public string Ma
-        {
-            get { return ma; }
-            set { ma = value; }
-        }
-        public string Ten
-        {
-            get { return ten; }
-            set { ten = value; }
-        }
-        public string DiaChi
-        {
-            get { return diaChi; }
-            set { diaChi = value; }
-        }
-        public int PhuCap
-        {
-            get { return phuCap; }
-            set { phuCap = value; }
-        }
-        public double HeSo
-        {
-            get { return heSo; }
-            set { heSo = value; }
-        }
+        public int PhuCap { get; set; }
+        public double HeSo { get; set; }
 
         // Thanh phan xu li
         public void NhapNhanVien(string ghiChu)
         {
-            Console.WriteLine(ghiChu);
-            Console.WriteLine("Nhap ma nhan vien: ");
-            this.ma = Console.ReadLine();
-            Console.WriteLine("Nhap ten nhan vien: ");
-            this.ten = Console.ReadLine();
-            Console.WriteLine("Nhap dia chi nhan vien: ");
-            this.diaChi = Console.ReadLine();
+            this.Nhap(ghiChu);
             Console.WriteLine("Nhap phu cap: ");
             this.phuCap = int.Parse(Console.ReadLine());
             Console.WriteLine("Nhap he so luong: ");
