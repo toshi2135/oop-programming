7b6ce4f654e7786e2a9910ef1df67309219a3ca4
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-14 16:01:17 +0700

    Create NHANVIEN.cs
    Create a class for inheritance

---
+++ oop-programming/NHANVIEN.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace oop_programming
+{
+    public class NHANVIEN
+    {
+        // Thanh phan du lieu
+        private string ma;
+        private string ten;
+        private string diaChi;
+
+        // Constructor
+        /*
+        public NVVP(string maNV, string tenNV, string diaChiNV, int phuCapNV, double heSoNV)
+        {
+            this.ma = maNV;
+            this.ten = tenNV;
+            this.diaChi = diaChiNV;
+            this.phuCap = phuCapNV;
+            this.heSo = heSoNV;
+        }
+        */
+
+        // Properties
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public string DiaChi { get; set; }
+
+        // Thanh phan xu li
+        public void Nhap(string ghiChu)
+        {
+            Console.WriteLine(ghiChu);
+            Console.WriteLine("Nhap ma nhan vien: ");
+            this.ma = Console.ReadLine();
+            Console.WriteLine("Nhap ten nhan vien: ");
+            this.ten = Console.ReadLine();
+            Console.WriteLine("Nhap dia chi nhan vien: ");
+            this.diaChi = Console.ReadLine();
+        }
+    }
+}
