e3a5e0b86dcef9560ccd460bb0db882ecace3097
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-17 02:39:43 +0700

    Create LuuTruSanPham.cs

---
+++ HDT.DataAccess/LuuTruSanPham.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using HDT.Entities;
+using Newtonsoft.Json;
+
+namespace HDT.DataAccess
+{
+    public class LuuTruSanPham
+    {
+        public List<Product> DocDanhSachSanPham()
+        {
+            StreamReader file = new StreamReader("/Users/tuan.nguyen/Documents/GitHub/oop-programming/data/products.json");
+            string jsonString = file.ReadToEnd();
+            file.Close();
+
+            var kq = JsonConvert.DeserializeObject<List<Product>>(jsonString);
+
+            return kq;
+        }
+    }
+}
