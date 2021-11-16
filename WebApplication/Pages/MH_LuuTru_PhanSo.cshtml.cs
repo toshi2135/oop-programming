e59026423c0ca4c08a32c0a5d5ddbc441dd36e99
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-16 21:40:09 +0700

    Separate Page & PageModel

--- WebApplication/Pages/MH_LuuTru_PhanSo.cshtml.cs
+++ WebApplication/Pages/MH_LuuTru_PhanSo.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HDT.Entities;
+using HDT.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,21 @@ namespace WebApplication.Pages
 {
     public class MH_LuuTru_PhanSoModel : PageModel
     {
+        // Se duoc hien thi voi GET method
         public void OnGet()
         {
+        }
+
+        // Se duoc hien thi voi POST method
+        public void OnPost()
+        {
+            int x = int.Parse(Request.Form["x"]);
+            int y = int.Parse(Request.Form["y"]);
+
+            PhanSo p = new PhanSo(x, y);
+
+            var xuLyPhanSo = new XuLyPhanSo();
+            xuLyPhanSo.LuuPhanSo(p);
         }
     }
 }
