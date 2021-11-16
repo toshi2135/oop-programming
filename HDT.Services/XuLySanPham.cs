953524509f20ce1a1374f9d11f2d63ee473d6edc
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-17 02:39:40 +0700

    Create XuLySanPham.cs

---
+++ HDT.Services/XuLySanPham.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using HDT.DataAccess;
+using HDT.Entities;
+
+namespace HDT.Services
+{
+    public class XuLySanPham
+    {
+        private LuuTruSanPham LuuTruSanPham { get; set; }
+        public XuLySanPham()
+        {
+            LuuTruSanPham = new LuuTruSanPham();
+        }
+
+        public List<Product> TimKiemSanPham()
+        {
+            var DsSanPham = LuuTruSanPham.DocDanhSachSanPham();
+        }
+    }
+}
