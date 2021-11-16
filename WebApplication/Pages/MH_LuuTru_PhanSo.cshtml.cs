c8d850d066e4f67cf257a4215e6554f09a099dd0
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-16 22:34:10 +0700

    Using [BindProperty]

--- WebApplication/Pages/MH_LuuTru_PhanSo.cshtml.cs
+++ WebApplication/Pages/MH_LuuTru_PhanSo.cshtml.cs
@@ -9,23 +9,35 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication.Pages
 {
+    // Xu ly du lieu dau vao, xac dinh du lieu cho Razor Views
     public class MH_LuuTru_PhanSoModel : PageModel
     {
+        [BindProperty]
+        public int X { get; set; }
+        [BindProperty]
+        public int Y { get; set; }
+        public string Chuoi { get; set; }
+
         // Se duoc hien thi voi GET method
         public void OnGet()
         {
+            X = 10;
+            Y = 20;
+            Chuoi = string.Empty;
         }
 
         // Se duoc hien thi voi POST method
         public void OnPost()
         {
-            int x = int.Parse(Request.Form["x"]);
-            int y = int.Parse(Request.Form["y"]);
+            // int x = int.Parse(Request.Form["x"]);
+            // int y = int.Parse(Request.Form["y"]);
 
-            PhanSo p = new PhanSo(x, y);
+            PhanSo p = new PhanSo(X, Y);
 
             var xuLyPhanSo = new XuLyPhanSo();
             xuLyPhanSo.LuuPhanSo(p);
+
+            Chuoi = "Da luu thanh cong";
         }
     }
 }
