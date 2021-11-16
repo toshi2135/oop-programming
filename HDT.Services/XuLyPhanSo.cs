91011c7b395dbf24700dc5e2791278f1697119fd
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-16 19:28:15 +0700

    Create XuLyPhanSo.cs

---
+++ HDT.Services/XuLyPhanSo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HDT.Entities;
+using HDT.DataAccess;
+
+namespace HDT.Services
+{
+    public class XuLyPhanSo
+    {
+        private LuuTruPhanSo LuuTruPhanSo { get; set; }
+
+        public XuLyPhanSo()
+        {
+            this.LuuTruPhanSo = new LuuTruPhanSo();
+        }
+
+        public void LuuPhanSo(XuLyPhanSo p)
+        {
+            LuuTruPhanSo.LuuPhanSo(p);
+        }
+    }
+}
