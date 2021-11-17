91e0da8434be0d536da0a195d8b59d951e0c5eee
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2021-11-17 13:56:41 +0700

    Update relative path

--- HDT.DataAccess/LuuTruSanPham.cs
+++ HDT.DataAccess/LuuTruSanPham.cs
@@ -11,13 +11,22 @@ namespace HDT.DataAccess
     {
         public List<Product> DocDanhSachSanPham()
         {
-            StreamReader file = new StreamReader("/Users/tuan.nguyen/Documents/GitHub/oop-programming/data/products.json");
+            StreamReader file = new StreamReader("./data/products.json");
             string jsonString = file.ReadToEnd();
             file.Close();
 
             var kq = JsonConvert.DeserializeObject<List<Product>>(jsonString);
 
             return kq;
+        }
+
+        public void LuuDanhSachSanPham(List<Product> dsSanPham)
+        {
+            StreamWriter file = new StreamWriter("./data/products.json");
+            string jsonString = JsonConvert.SerializeObject(dsSanPham);
+            file.Write(jsonString);
+
+            file.Close();
         }
 
         public void ThemSanPham(Product p)
@@ -31,14 +40,7 @@ namespace HDT.DataAccess
             LuuDanhSachSanPham(dsSanPham);
         }
 
-        public void LuuDanhSachSanPham(List<Product> dsSanPham)
-        {
-            StreamWriter file = new StreamWriter("/Users/tuan.nguyen/Documents/GitHub/oop-programming/data/products.json");
-            string jsonString = JsonConvert.SerializeObject(dsSanPham);
-            file.Write(jsonString);
 
-            file.Close();
-        }
 
         public Product DocSanPham(int id)
         {
